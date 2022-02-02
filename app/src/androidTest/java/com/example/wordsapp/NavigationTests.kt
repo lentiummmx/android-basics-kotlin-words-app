085394d50d5de8dcfab264f78d9a31cbce27cfa7
lentiummmx <lentium.mmx@gmail.com>
2022-02-01 18:28:05 -0600

    solution_activities File(s) committed to updating code for instrumental testing, navigation test (v0.1.0)

---
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -0,0 +1,35 @@
+package com.example.wordsapp
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTests {
+
+    @get:Rule
+    val activity: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun navigate_to_word_by_position() {
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+        onView(withText("Words That Start With C")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navigate_to_word_by_text() {
+        onView(withText("C")).perform(click())
+        onView(withText("Words That Start With C")).check(matches(isDisplayed()))
+    }
+
+}
